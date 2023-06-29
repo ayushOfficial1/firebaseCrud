a3166024b4a213c98cb24100c87c9e4da0fe5efd
Ayush Gupta <19ec1038@mitsgwl.ac.in>
2023-06-29 13:40:55 +0530

    erased sensitive info

--- src/firebase-auth.js
+++ src/firebase-auth.js
@@ -5,21 +5,6 @@ require("dotenv").config();
 
 import { GoogleAuthProvider, getAuth } from "firebase/auth";
 
-// const firebaseConfig = {
-// //   apiKey: "AIzaSyDY0yXXGsAteEiKYAK2LgwHffvNDRDV25M",
-// //   authDomain: "todo-bce57.firebaseapp.com",
-// //   projectId: "todo-bce57",
-// //   storageBucket: "todo-bce57.appspot.com",
-// //   messagingSenderId: "713928920854",
-// //   appId: "1:713928920854:web:19433b710354be3e968a37",
-//   process.env.API_KEY=AIzaSyDY0yXXGsAteEiKYAK2LgwHffvNDRDV25M
-//   process.env.AUTH_DOMAIN=todo-bce57.firebaseapp.com
-//   process.env.PROJECT_ID=todo-bce57
-//   process.env.STORAGE_BUCKET=todo-bce57.appspot.com
-//   process.env.MESSAGING_SENDER_ID=713928920854
-//   process.env.APP_ID=1:713928920854:web:19433b710354be3e968a37
-
-// };
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
